253dd3c7ff065b6b5f8cba1555b586a0101be773
gerry <blog@gerrywen.com>
2020-05-22 00:08:25 +0800

    file

---
+++ flinkx-file/flinkx-file-reader/src/main/java/com/dtstack/flinkx/file/reader/FileInputFormatBuilder.java
@@ -0,0 +1,16 @@
+package com.dtstack.flinkx.file.reader;
+
+import com.dtstack.flinkx.inputformat.BaseRichInputFormatBuilder;
+
+/**
+ * program: flinkx-all->FileInputFormatBuilder
+ * description:
+ * author: gerry
+ * created: 2020-05-21 21:39
+ **/
+public class FileInputFormatBuilder extends BaseRichInputFormatBuilder{
+    @Override
+    protected void checkFormat() {
+
+    }
+}
